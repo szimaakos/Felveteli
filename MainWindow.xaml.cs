3c4fccc8c35cefdfeb07ab44dfcea60088930281
Hunor József Nagy <Nhunor06@gmail.com>
2024-01-16 14:40:08 +0100

    1.0

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace felveteli
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> betolt = new();
+        List<Transform> transforms = new();
+        
+            
+        private Transport transport ;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using(StreamReader sr = new StreamReader("felveteli.csv"))
+            {
+
+            }
+
+
+
+        }
+
+        private void btnUjDiak_Click(object sender, RoutedEventArgs e)
+        {
+
+            transport =new Transport();
+            UjDiak ujDiak = new UjDiak(transport);
+            ujDiak.ShowDialog();
+
+        }
+
+        private void btnTorles_Click(object sender, RoutedEventArgs e)
+        {
+            if (dtgFelveteli.SelectedItem != null)
+            {
+                dtgFelveteli.Items.Remove(dtgFelveteli.SelectedItem);
+            }
+
+        }
+
+        private void btnImport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public void LoadElements()
+        {
+
+            foreach (var item in betolt)
+            {
+                
+            }
+        }
+    }
+}
---
+++ UjDiak.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace felveteli
+{
+    /// <summary>
+    /// Interaction logic for UjDiak.xaml
+    /// </summary>
+    /// 
+    public partial class UjDiak : Window
+    {
+        public UjDiak(Transport move)
+        {
+            InitializeComponent();
+            move.OMAzonosito = txbOMAzonosito.Text;
+            move.Nev = txbNev.Text;
+            move.ErtesitesiCim = txbErtesiteiCim.Text;
+            move.SzuletesiDatum = txbSzuletesiDatum.Text;
+            move.ElerhetosegEmail = txbElerhetosegEmail.Text;
+            try
+            {
+                move.MagyarPontszam = int.Parse(txbMagyarPontszam.Text);
+
+            }
+            catch (Exception)
+            {
+                move.MagyarPontszam = -1;
+            }
+            try
+            {
+                move.MatekPontszam = int.Parse(txbMatekPontszam.Text);
+
+            }
+            catch (Exception)
+            {
+                move.MatekPontszam = -1;
+            }
+            move.Telefon = txbTelefon.Text;
+            move.Iskola = txbIskola.Text;
+            try
+            {
+                move.Konnyites = byte.Parse(txbKonnyites.Text);
+
+            }
+            catch (Exception)
+            {
+                move.Konnyites = 0;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
